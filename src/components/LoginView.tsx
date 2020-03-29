db6a2ee25dd0a7dec1ec2b7400300ccd2c9c3ede
Mike Joyce <mjoyce91@gmail.com>
2020-03-29 14:22:18 -0400

    Remove deprecated classes/divs

--- src/components/LoginView.tsx
+++ src/components/LoginView.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import AuthModal from './AuthModal';
 
 const LoginView: React.FC = () => {
   return (
-    <div>
-      <div className="container">
-        <div className="c-intro">
-        </div>
-        <AuthModal />
-      </div>
-    </div>
+    <Container>
+      <AuthModal />
+    </Container>
   );
 };
 
