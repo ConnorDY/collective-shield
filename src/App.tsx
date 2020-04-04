50e08a612a26f971ac081b7fda675314f3b0a7c0
Connor Younglund <younglund_connor@bah.com>
2020-04-04 13:04:38 -0400

    don't check error status code

--- src/App.tsx
+++ src/App.tsx
@@ -33,10 +33,7 @@ const App: React.FC = () => {
         setUser(res.data);
       })
       .catch((err) => {
-        if (get(err, 'response.status') === 401) {
-          history.push('/login');
-          return null;
-        }
+        history.push('/login');
       });
   }, []);
 
