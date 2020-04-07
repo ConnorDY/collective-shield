a6dd91012b17039427778975858a0066b6d30404
Kate Kuratsu <katekuratsu@kates-mbp.gateway.ht.net>
2020-04-07 07:41:46 -1000

    Update role and login buttons to full width of container

--- ui/src/components/AuthModal.tsx
+++ ui/src/components/AuthModal.tsx
@@ -3,7 +3,8 @@ import {
   Button,
   Container,
   Modal,
-  Row
+  Row,
+  Col
   // ToggleButton,
   // ToggleButtonGroup
 } from 'react-bootstrap';
@@ -48,22 +49,26 @@ const HomeView: React.FC<{}> = () => {
             */}
           <Container>
             <Row className="justify-content-md-center p-2">
-              <Button
-                disabled={registerDisabled}
-                variant="primary"
-                href="/login/facebook"
-                className="mr-2 mb-2"
-              >
-                {buttonPreText} with Facebook
-              </Button>{' '}
-              <Button
-                disabled={registerDisabled}
-                variant="primary"
-                href="/login/google"
-                className="mb-2"
-              >
-                {buttonPreText} with Google
-              </Button>{' '}
+              <Col sm={6}>
+                <Button
+                  disabled={registerDisabled}
+                  variant="primary"
+                  href="/login/facebook"
+                  className="mr-2 mb-2 btn-block"
+                >
+                  {buttonPreText} with Facebook
+                </Button>{' '}
+              </Col>
+              <Col sm={6}>
+                <Button
+                  disabled={registerDisabled}
+                  variant="primary"
+                  href="/login/google"
+                  className="mb-2 btn-block"
+                >
+                  {buttonPreText} with Google
+                </Button>{' '}
+              </Col>
             </Row>
           </Container>
           {/*
