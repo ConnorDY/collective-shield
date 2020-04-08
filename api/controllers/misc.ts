c653cb444a820613c02a5a765a1d4aae68ea515d
Connor Younglund <younglund_connor@bah.com>
2020-04-08 09:07:27 -0400

    remove unused schemas/code

--- api/controllers/misc.ts
+++ api/controllers/misc.ts
@@ -5,26 +5,14 @@ import {
   CurrentUser
 } from 'routing-controllers';
 
-import { Maker } from '../schemas';
-import { IMaker, IUser } from '../interfaces';
+import { IUser } from '../interfaces';
 
 @JsonController()
 export default class MiscController {
   @Authorized()
   @Get('/api/me')
   getMyProfile(@CurrentUser() user: IUser) {
-    if (!user._id) {
-      return user;
-    }
-
-    return Maker.findById(user._id)
-      .then((result) => {
-        user.maker = result as IMaker;
-        return user;
-      })
-      .catch((err) => {
-        throw err;
-      });
+    return user;
   }
 
   @Get('/ping')
