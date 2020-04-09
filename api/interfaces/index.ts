7766e3c21343d05fc485cd0097892b639c9b9f46
Connor Younglund <younglund_connor@bah.com>
2020-04-09 15:38:36 -0400

    remove unused interface

--- api/interfaces/index.ts
+++ api/interfaces/index.ts
@@ -1,4 +1,3 @@
-export { default as IAddress } from './address';
 export { default as IOauthProviders } from './oauth-providers';
 export { default as IRequest } from './request';
 export { default as IUser } from './user';
