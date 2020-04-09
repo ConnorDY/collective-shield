c005786018e93e3056d38ee7f7dca5eeb92ff88e
Connor Younglund <younglund_connor@bah.com>
2020-04-09 17:31:51 -0400

    update User model

--- ui/src/models/User.ts
+++ ui/src/models/User.ts
@@ -2,8 +2,21 @@ export default interface User {
   _id: string;
   firstName: string;
   lastName: string;
+  email: string;
   maker?: boolean;
   isSuperAdmin?: boolean;
-  roles: string[];
-  email: string;
+  isVerifiedMaker?: boolean;
+  makerDetails?: {
+    firstName: string;
+    lastName: string;
+    addressLine1: string;
+    addressLine2?: string;
+    city: string;
+    state: string;
+    zip: string;
+    phone: string;
+    homePickUp: boolean;
+    willShip: boolean;
+    willDeliver: boolean;
+  };
 }
