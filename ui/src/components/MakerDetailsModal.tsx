f2d9055f666cc181dba89b1a9e2cd14e77ff00bd
Connor Younglund <younglund_connor@bah.com>
2020-04-11 12:43:48 -0400

    create MakersDetailsModal

---
+++ ui/src/components/MakerDetailsModal.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Button, Container, Modal, Row, Col } from 'react-bootstrap';
+
+import User from '../models/User';
+
+const MakerDetailsModal: React.FC<{
+  user: User;
+  onClose: () => void;
+}> = ({ user, onClose }) => {
+  return (
+    <Modal show size="lg" onHide={onClose} backdrop>
+      <div className="p-3">
+        <Modal.Header>
+          <Modal.Title>Maker Details</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Container>
+            <Row className="justify-content-md-center p-2">
+              <Col sm={6} lg={3}>
+                <Button className="btn-block" onClick={onClose}>
+                  Close
+                </Button>
+              </Col>
+
+              <Col sm={6} lg={3}>
+                <Button className="btn-block">Approve</Button>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Footer>
+      </div>
+    </Modal>
+  );
+};
+
+export default MakerDetailsModal;
--- ui/src/views/MakersView.tsx
+++ ui/src/views/MakersView.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Row, Jumbotron } from 'react-bootstrap';
+import { Button, Col, Row, Jumbotron } from 'react-bootstrap';
 import axios from 'axios';
 
-import { buildEndpointUrl } from '../utilities';
 import User from '../models/User';
+import MakerDetailsModal from '../components/MakerDetailsModal';
+import { buildEndpointUrl } from '../utilities';
 
 export default function MakersView() {
   // const [approved, setApproved] = useState<User[]>([]);
   const [unapproved, setUnapproved] = useState<User[]>([]);
+  const [modalUser, setModalUser] = useState<User>();
 
   // function getApproved() {
   //   axios
@@ -21,6 +23,14 @@ export default function MakersView() {
     axios.get(buildEndpointUrl('makers/unapproved')).then((res) => {
       setUnapproved(res.data);
     });
+  }
+
+  function viewDetails(user: User) {
+    setModalUser(user);
+  }
+
+  function closeModal() {
+    setModalUser(undefined);
   }
 
   useEffect(() => {
@@ -46,28 +56,38 @@ export default function MakersView() {
             </Col>
           ) : (
             <Col>
-              <table className="makers-table">
-                <thead>
-                  <tr>
-                    <th className="name">Name</th>
-                    <th className="email">Email</th>
-                    <th className="view-details">
-                      <span className="sr-only">View Details</span>
-                    </th>
-                  </tr>
-                </thead>
-              </table>
-              {unapproved.map(({ _id, email, makerDetails }, key) => {
-                return (
-                  <tr key={key}>
-                    <td>
-                      {makerDetails!.firstName} {makerDetails!.lastName}
-                    </td>
-                    <td>{email}</td>
-                    <td>Button to view details here</td>
-                  </tr>
-                );
-              })}
+              <div className="table-wrapper">
+                <table className="makers-table">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>
+                        <span className="sr-only">View Details</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {unapproved.map(({ email, makerDetails }, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>
+                            {makerDetails!.firstName} {makerDetails!.lastName}
+                          </td>
+                          <td>{email}</td>
+                          <td className="view-details">
+                            <Button
+                              onClick={() => viewDetails(unapproved[index])}
+                            >
+                              View Details
+                            </Button>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
             </Col>
           )}
         </Row>
@@ -84,36 +104,47 @@ export default function MakersView() {
         <Row>
           {!approved || !approved.length ? (
             <Col>
-              <Jumbotron className="text-center">No approved makers found.</Jumbotron>
+              <Jumbotron className="text-center">
+                No approved makers found.
+              </Jumbotron>
             </Col>
           ) : (
             <Col>
-              <table className="makers-table">
-                <thead>
-                  <tr>
-                    <th className="name">Name</th>
-                    <th className="email">Email</th>
-                    <th className="view-details">
-                      <span className="sr-only">View Details</span>
-                    </th>
-                  </tr>
-                </thead>
-              </table>
-              {approved.map(({ _id, email, makerDetails }, key) => {
-                return (
-                  <tr key={key}>
-                    <td>
-                      {makerDetails!.firstName} {makerDetails!.lastName}
-                    </td>
-                    <td>{email}</td>
-                    <td>Button to view details here</td>
-                  </tr>
-                );
-              })}
+              <div>
+                <table className="makers-table">
+                  <thead>
+                    <tr>
+                      <th className="name">Name</th>
+                      <th className="email">Email</th>
+                      <th className="view-details">
+                        <span className="sr-only">View Details</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {approved.map(({ _id, email, makerDetails }, key) => {
+                      return (
+                        <tr key={key}>
+                          <td>
+                            {makerDetails!.firstName} {makerDetails!.lastName}
+                          </td>
+                          <td>{email}</td>
+                          <td>Button to view details here</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
             </Col>
           )}
         </Row>
-      </div>*/}
+      </div>
+      */}
+
+      {modalUser && (
+        <MakerDetailsModal onClose={() => closeModal()} user={modalUser} />
+      )}
     </>
   );
 }
