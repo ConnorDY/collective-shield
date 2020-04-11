1c817d97b83bbf2abc4c0d180054eaed51b19aa7
Connor Younglund <younglund_connor@bah.com>
2020-04-11 14:31:47 -0400

    add email content

--- api/controllers/makers.ts
+++ api/controllers/makers.ts
@@ -48,8 +48,40 @@ export default class MakersController {
       .then((user) => {
         return sendEmail({
           to: user.email,
-          subject: "You've been approved",
-          body: 'Placeholder body'
+          subject: 'Welcome to the Collective Shield community of makers!',
+          body: `<p>
+            Dear ${user.firstName} ${user.lastName},
+          </p>
+
+          <p>
+            We are so pleased you joined us to create these much-needed items for those on frontlines of the battle against coronavirus.
+            This is a very engaged community and we welcome your participation.
+          </p>
+          
+          <p>
+            Please always remember to follow our guiding principles and treat those who request your products with the upmost care.
+          </p>
+
+          <p>
+            Join our
+            <a href="https://join.slack.com/t/3dprintedshields-tech/shared_invite/zt-cx21coci-T28PdkLkZDdRIAKih825Zw">
+              slack workspace
+            </a>
+            to share design suggestions, get help with technical maker problems, and collaborate on new solutions
+            (for issues connecting to slack, email
+            <a href="mailto:amy@collectiveshield.org">Amy@CollectiveShield.org</a>
+            ).
+          </p>
+
+          <p style="font-style: italic;">
+            <span style="font-weight: bold">Note:</span>
+            When you return to the Collective Shield website, you may need to log out and log back in to activate your account.
+          </p>
+
+          <p>
+            We are all in this together.<br />
+            <span style="font-weight: bold">The Collective Shield team</span>
+          </p>`
         })
           .then(() => {
             return user;
