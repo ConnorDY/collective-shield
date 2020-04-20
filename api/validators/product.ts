6110a218b9abdf79ee08859030688a442aca1a5f
Mike Joyce <mjoyce91@gmail.com>
2020-04-20 13:25:36 -0400

    Refactor the product validator

--- api/validators/product.ts
+++ api/validators/product.ts
@@ -1,21 +1,19 @@
 import { Joi } from 'celebrate';
 
-const productValidator = Joi.object().keys({
+const productKeys = {
   name: Joi.string().required(),
   description: Joi.string().allow('').optional(),
   imageUrl: Joi.string().allow('').optional(),
   packingUrl: Joi.string().allow('').optional(),
   modelUrl: Joi.string().allow('').optional(),
   isArchived: Joi.boolean().optional()
-});
+};
+
+const productValidator = Joi.object().keys(productKeys);
 
 export const productPatchValidator = Joi.object().keys({
-  name: Joi.string().optional(),
-  description: Joi.string().allow('').optional(),
-  imageUrl: Joi.string().allow('').optional(),
-  packingUrl: Joi.string().allow('').optional(),
-  modelUrl: Joi.string().allow('').optional(),
-  isArchived: Joi.boolean().optional()
+  ...productKeys,
+  name: Joi.string().optional()
 });
 
 export default productValidator;
