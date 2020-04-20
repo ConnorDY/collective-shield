4615f8eddfc16da639400c8bd0e03c155917022d
ath0111 <56038771+ath0111@users.noreply.github.com>
2020-04-20 14:22:48 -0400

    fixes date format order

--- ui/src/utilities/formatDate.ts
+++ ui/src/utilities/formatDate.ts
@@ -1,4 +1,4 @@
 import moment from 'moment';
 
-export const formatDate = (date: Date, format = 'DD/MM/YY') =>
+export const formatDate = (date: Date, format = 'MM/DD/YY') =>
   moment(date).format(format);
